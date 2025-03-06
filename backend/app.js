e6bb7e43715329b3abcaf78bd8f5675e743b5f18
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2025-03-06 15:17:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://job-seeking-frontend-peel.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
